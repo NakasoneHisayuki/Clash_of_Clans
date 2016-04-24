f0a19ec48929be89b6eaacbfa17fafde118b41d7
NakasoneHisayuki <hsyknksn@gmail.com>
2016-04-25 00:44:40 +0900

    コメントアウト取り消し

--- Clash_of_Clans/Assets/script/Astar.cs
+++ Clash_of_Clans/Assets/script/Astar.cs
@@ -55,10 +55,10 @@ public class Astar : MonoBehaviour {
 		{ 1, "バナナ" },
 		{ 2, "チョコレート" },
 	};
-//	private List<int> intList = new List<int>();
-//	private List<Dictionary<int, string>> intList = new List<Dictionary<int, string>>(){
-//		{ 0{0, "test"} },
-//	};
+	private List<int> intList = new List<int>();
+	private List<Dictionary<int, string>> intList = new List<Dictionary<int, string>>(){
+		{ 0{0, "test"} },
+	};
 
 
 
